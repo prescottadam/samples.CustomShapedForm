a5ec4fc36028b20455c9dd9245816a0f9a552e02
Adam Prescott <prescott.adam@gmail.com>
2012-10-17 08:58:01 -0400

    Sample with custom shaped form

---
+++ samples.CustomShapedForm/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace samples.CustomShapedForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.uxClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uxClose
+            // 
+            this.uxClose.Location = new System.Drawing.Point(62, 54);
+            this.uxClose.Name = "uxClose";
+            this.uxClose.Size = new System.Drawing.Size(75, 23);
+            this.uxClose.TabIndex = 0;
+            this.uxClose.Text = "Close";
+            this.uxClose.UseVisualStyleBackColor = true;
+            this.uxClose.Click += new System.EventHandler(this.uxClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(164, 176);
+            this.Controls.Add(this.uxClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uxClose;
+    }
+}
+
---
+++ samples.CustomShapedForm/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace samples.CustomShapedForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var bitmap = new Bitmap("ninja.png");
+            Region = GetRegion(bitmap);
+        }
+
+        private Region GetRegion(Bitmap _img)
+        {
+            var rgn = new Region();
+            rgn.MakeEmpty();
+            var rc = new Rectangle(0, 0, 0, 0);
+            bool inimage = false;
+            for (int y = 0; y < _img.Height; y++)
+            {
+                for (int x = 0; x < _img.Width; x++)
+                {
+                    if (!inimage)
+                    {
+                        // if pixel is not transparent
+                        if (_img.GetPixel(x, y).A != 0)
+                        {
+                            inimage = true;
+                            rc.X = x;
+                            rc.Y = y;
+                            rc.Height = 1;
+                        }
+                    }
+                    else
+                    {
+                        // if pixel is transparent
+                        if (_img.GetPixel(x, y).A == 0)
+                        {
+                            inimage = false;
+                            rc.Width = x - rc.X;
+                            rgn.Union(rc);
+                        }
+                    }
+                }
+                if (inimage)
+                {
+                    inimage = false;
+                    rc.Width = _img.Width - rc.X;
+                    rgn.Union(rc);
+                }
+            }
+            return rgn;
+        }
+
+        private bool _mouseDown;
+        private Point _startPoint;
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _mouseDown = true;
+            _startPoint = new Point(e.X, e.Y);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _mouseDown = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_mouseDown)
+            {
+                var p1 = new Point(e.X, e.Y);
+                var p2 = PointToScreen(p1);
+                var p3 = new Point(p2.X - _startPoint.X,
+                                     p2.Y - _startPoint.Y);
+                Location = p3;
+            }
+        }
+
+        private void uxClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
